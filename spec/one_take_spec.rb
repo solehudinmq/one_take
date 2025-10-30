06119a54f8d2e366e9d3af30f62aa4e6f87662df
SolehMQ <solehudinmq@gmail.com>
2025-10-31 00:58:56 +0700

    fix spec

--- spec/one_take_spec.rb
+++ spec/one_take_spec.rb
@@ -2,7 +2,6 @@
 
 # run : 
 # redis-server --port 6380
-# bundle exec ruby app.rb
 RSpec.describe OneTake do
   before(:all) do
     Post.delete_all
@@ -16,28 +15,29 @@ RSpec.describe OneTake do
   end
 
   it "return successful, when retrying the data is not duplicated" do
-    result = HTTParty.post('http://localhost:4567/posts', 
-      body: { title: "Post 1", content: "Content post 1" }.to_json,
-      headers: { 'x-idempotency-key': 'abc-1', 'Content-Type' => 'application/json' },
-      timeout: 3
-    )   
-      
-    data = result['data']
+    idempotency_key = 'abc-1'
+
+    idempotency = OneTake::Idempotency.new
+    result = idempotency.perform(idempotency_key: idempotency_key) do
+      post = Post.create(title: 'Post 1', content: 'Content post 1')
+
+      post
+    end
     
-    expect(result.code).to be(201)
+    data = JSON.parse(result['data'])
+
     expect(data["title"]).to eq('Post 1')
     expect(data["content"]).to eq('Content post 1')
 
     # retry
-    result2 = HTTParty.post('http://localhost:4567/posts', 
-      body: { title: "Post 1", content: "Content post 1" }.to_json,
-      headers: { 'x-idempotency-key': 'abc-1', 'Content-Type' => 'application/json' },
-      timeout: 3
-    )   
-      
-    data2 = result2['data']
+    result2 = idempotency.perform(idempotency_key: idempotency_key) do
+      post = Post.create(title: 'Post 1', content: 'Content post 1')
+
+      post
+    end
     
-    expect(result2.code).to be(201)
+    data2 = JSON.parse(result2['data'])
+
     expect(data2["title"]).to eq('Post 1')
     expect(data2["content"]).to eq('Content post 1')
 
@@ -46,24 +46,32 @@ RSpec.describe OneTake do
   end
 
   it "return failed, x-idempotency-key header not sent" do
-    result = HTTParty.post('http://localhost:4567/posts', 
-      body: { title: "Post 2", content: "Content post 2" }.to_json,
-      headers: { 'Content-Type' => 'application/json' },
-      timeout: 3
-    )
+    begin
+      idempotency_key = nil
 
-    expect(result.code).to be(500)
-    expect(result["error"]).to eq("header 'x-idempotency-key' is required to be sent.")
+      idempotency = OneTake::Idempotency.new
+      result = idempotency.perform(idempotency_key: idempotency_key) do
+        post = Post.create(title: 'Post 2', content: 'Content post 2')
+
+        post
+      end
+    rescue => e
+      expect(e.message).to eq("header 'x-idempotency-key' is required to be sent.")
+    end
   end
 
   it "return failed, due to error when saving data" do
-    result = HTTParty.post('http://localhost:4567/error_simulations', 
-      body: { title: "Post 2", content: "Content post 2" }.to_json,
-      headers: { 'x-idempotency-key': 'abc-2', 'Content-Type' => 'application/json' },
-      timeout: 3
-    )
-    
-    expect(result.code).to be(500)
-    expect(result["error"]).to eq("Failed to lock : Failure occurred while saving data.")
+    begin
+      idempotency_key = 'abc-1'
+
+      idempotency = OneTake::Idempotency.new
+      result = idempotency.perform(idempotency_key: idempotency_key) do
+        post = Post.create(title: nil, content: 'Content post 1')
+
+        post
+      end
+    rescue => e
+      expect(e.message).to eq("Failed to lock : Failure occurred while saving data.")
+    end
   end
 end
